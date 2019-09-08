692506e066b9b55828ef27482a6660a4a917cfd6
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-08 16:18:13 +0700

    Avoiding Mutations

---
+++ js/04/script-02.js
@@ -0,0 +1,14 @@
+//Avoiding Mutations
+"use strict";
+const arr = [3,4,2,5,1,6];
+Object.freeze(arr);
+
+const sortArray = function(arr1) {
+    return arr1.sort();
+};
+//not allow sort
+
+const newNums = sortArray(arr);
+
+console.log(newNums);
+console.log(arr);
