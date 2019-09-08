b4ebbabb0899b6d511ba326256fddd5b4d4de295
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-08 15:52:37 +0700

    update

--- js/04/script.js
+++ js/04/script.js
@@ -1,42 +1,33 @@
-
 users = [
   { name: "James", score: 30, tries: 1 },
   { name: "Mary", score: 110, tries: 4 },
   { name: "Henry", score: 80, tries: 3 }
 ];
 
-//modify data
-var storeUser  = function (arr, user) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].name.toLowerCase() === user.name.toLowerCase()) {
-      arr[i] = user;
-      break;
+
+//Oher Possible Solutions and the Problems
+var newScore = function(arr, name, amt) {
+  arr.forEach(function(val) {
+    if (val.name.toLowerCase() === name.toLowerCase()) {
+      val.score = val.score + amt;
     }
-  }
-}
+  });
+  return arr;
+};
 
-//pure
-var getUser = function(arr, name) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].name.toLowerCase() === name.toLowerCase()) {
-      return arr[i];
+var newTries = function(arr, name) {
+  arr.forEach(function(val) {
+    if (val.name.toLowerCase() === name.toLowerCase()) {
+      val.tries++;
     }
-  }
-
-  return null;
+  });
+  return arr;
 };
 
-var updateScore = function(user, newAmt) {
-  if (user) {
-    users.score += newAmt;
-    return user;
-  }
-};
+var newArray1 = newScore(users, "Henry", 30);
+var newArray2 = newTries(users, "Henry"); //same newArray1
 
-var updateTries = function(user) {
-  if (user) {
-    users.tries++;
-    return user;
-  }
-};
+
+
+var users = [{name: "James",score: 30,tries: 1}, {name: "Mary", score: 110,tries: 4}, {name: "Henry",score: 80,tries: 3}];
 
