91088fc92faa89073502fbdc343083a8080a9c75
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-10 02:30:13 +0700

    Declarative Programming

---
+++ js/06/script.js
@@ -0,0 +1,15 @@
+
+function reduceFunc () {
+  return fns.reduce(function(v, f) {
+    return f(v);
+  }, x);
+} 
+
+let result;
+for (let i = 0; i < fns.length; i++) {
+  if (i === 0) {
+      result = fns[i](x);
+  }
+  result = fns[i](result);
+}
+return result;
