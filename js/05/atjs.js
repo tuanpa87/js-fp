accd7662f1fe26253a6517ee9cb51c8d43df8720
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-09-10 01:44:25 +0700

    currying

--- js/05/atjs.js
+++ js/05/atjs.js
@@ -1,17 +1,61 @@
+// const pipe = function(...fns) {
+//   return function(x) {
+//     return fns.reduce(function(v, f) {
+//       return f(v);
+//     }, x);
+//   };
+// };
 
+// const compose = function(...fns) {
+//   return function(x) {
+//     return fns.reduceRight(function(v, f) {
+//       return f(v);
+//     }, x);
+//   };
+// };
 
-const pipe = function(...fns) {
-  return function(x) {
-      return fns.reduce(function(v, f) {
-          return f(v);
-      }, x);
-  }
-};
+// function curry(fn, arity = fn.length) {
+//   return (function nextCurried(prevArgs) {
+//     return function curried(nextArg) {
+//       console.log(prevArgs);
+//       console.log(nextArg);
+//       var args = [...prevArgs, nextArg];
+//       if (args.length >= arity) {
+//         return fn(...args);
+//       } else {
+//         return nextCurried(args);
+//       }
+//     };
+//   })([]);
+// }
 
-const compose = function(...fns) {
-  return function(x) {
-      return fns.reduceRight(function(v, f) {
-          return f(v);
-      }, x);
-  }
-};
+// function curry(fn, arity = fn.length) {
+//   return (function nextCurried(prevArgs) {
+//     return function curried(nextArg) {
+//       var args = [...prevArgs, nextArg];
+//       if (args.length >= arity) {
+//         return fn(...args);
+//       } else {
+//         return nextCurried(args);
+//       }
+//     };
+//   })([]);
+// }
+
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+
+const curry = (fn, arity = fn.length, nextCurried) =>
+  (nextCurried = prevArgs => nextArg => {
+    var args = [...prevArgs, nextArg];
+    //console.log(args);
+    return args.length >= arity
+      ? fn(...args)
+      : nextCurried(args);
+  })([]);
+
+// const curry = (fn, arity = fn.length, nextCurried) =>
+//   (nextCurried = prevArgs => nextArg =>
+//     [...prevArgs, nextArg].length >= arity
+//       ? fn(...[...prevArgs, nextArg])
+//       : nextCurried([...prevArgs, nextArg]))([]);
---
+++ js/05/script-05.js
@@ -0,0 +1,74 @@
+const curryGreeting = function(greeting) {
+  return function(name) {
+    console.log(greeting + " " + name);
+  };
+};
+
+const welcomeGreet = curryGreeting("Welcome");
+
+//welcomeGreet("Steve");
+//welcomeGreet("Mary");
+
+///////////////////////////////////////
+
+const ffun = function(a, b, c) {
+  return a + b + c;
+};
+
+const gfun = function(d, e) {
+  return d + e;
+};
+
+const hfun = function(f, g, h) {
+  return f + g + h;
+};
+
+/*const curriedF = curry(ffun);
+const curriedG = curry(gfun);
+const curriedH = curry(hfun);
+
+const newFun = pipe (
+    curriedF(1)(2),
+    curriedG(4),
+    curriedH(5)(6));*/
+
+const newFun = pipe(
+  curry(ffun)(1)(2),
+  curry(gfun)(4),
+  curry(hfun)(5)(6)
+);
+
+const doubleNum = function(num) {
+  return num + num;
+};
+
+const totalIt = function(n1, n2, n3, n4) {
+  return n1 + n2 + n3 + n4;
+};
+
+const doArray = function(num1, num2) {
+  return [num1, num2];
+};
+
+const newFunction = pipe(
+  doubleNum,
+  curry(totalIt)(3)(2)(1),
+  curry(doArray)(50)
+);
+
+//////////////////////////////////
+
+const curriedTotalIt = curry(totalIt);
+const curriedDoArray = curry(doArray);
+
+/*const newFunction = pipe(
+    doubleNum,
+    curriedTotalIt(3)(2)(1),
+    curriedDoArray(50));*/
+
+    // let fn1 = curriedTotalIt(5);
+    // console.log(fn1);
+    // let fn2 = fn1(6);
+    // console.log(fn2);
+    // let fn3 = fn2(7);
+    // let fn4 = fn3(8);
